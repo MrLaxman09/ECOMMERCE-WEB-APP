f78ed14f38b8c15417a457b8eaa3b1d41d01f520
Laxman Kumar <laxmankumar6089@gmail.com>
2026-01-14 16:47:04 +0530

    Update API endpoints for login and signup

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -13,7 +13,7 @@ const LoginSignup = () => {
   const login = async () =>{
     console.log("login", formData)
     let responseData;
-    await fetch('http://localhost:4000/login', {
+    await fetch('https://ecommerce-web-app-oe7k.onrender.com/login', {
       method: 'POST',
       headers:{
         Accept:'application/form-data',
@@ -32,7 +32,7 @@ const LoginSignup = () => {
   const signup = async () =>{
     console.log("signup", formData)
     let responseData;
-    await fetch('http://localhost:4000/signup', {
+    await fetch('https://ecommerce-web-app-oe7k.onrender.com/signup', {
       method: 'POST',
       headers:{
         Accept:'application/form-data',
